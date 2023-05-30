02eb49bfb16adfff64c0154fcfdd0233ef7b9f57
Yurster-Yilson <yurik17021@gmail.com>
2023-05-30 12:24:27 +0300

    LR3

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,28 @@
 const express = require("express")
 const app = express()
+const {port, host} = require("./configuration")
+const {connectDB} = require("./utils/db")
+const {User} = require("./models/user")
 
-app.get("/test", (req, res) => {
-    res.send("Server is working correctly")
-})
+connectDB()
+    .on('error', console.error.bind(console, 'connection error:'))
+    .once("open", startServer)
 
-app.listen(8080, () => {
-    console.log("API-service is working")
-})
+function startServer() {
+    app.listen(port, () => {
+        console.log(`API-service is working on ${host}:${port}`)
+    })
+    app.get("/", (req, res) => {
+        res.send(`Server is working correctly`)
+    })
+    app.get('/users', async (req, res) => {
+        try {
+            const user = new User({firstName: "Yurii", lastName: "Popad"})
+            await user.save()
+            const users = await User.find()
+            res.json({users})
+        } catch (err) {
+            res.send({err})
+        }
+    })
+}
